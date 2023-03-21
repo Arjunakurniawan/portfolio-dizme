097a4e2472dc219b1d93d4783fb2702f6d218f2d
Arjunakurniawan <arjunakrn876@gmail.com>
2023-03-20 23:22:31 -0600

    sudah sampai membuat components

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+function Hero() {
+  return (
+    <section>
+      <div className="col-6">
+        <p>Hallo, I am</p>
+        <h3>Arjuna Kurniawan</h3>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ src/components/Navbar.js
@@ -0,0 +1,64 @@
+import React from "react";
+import logo from ".././assets/asset 0.png";
+
+function Navbar() {
+  return ( 
+    <section>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-md">
+          <img src={logo} className="logo" />
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            id="navbarNav"
+          >
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" href="#">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Portfolio
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Service
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Blog
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </section>
+  );
+}
+
+export default Navbar;
